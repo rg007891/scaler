c7ffd039149d3d590ad80e68e25216575d2c2ca7
Rohit <rg007891@gmail.com>
2024-06-28 13:02:47 +0530

    A.java update by local in line 6

--- A.java
+++ A.java
@@ -3,5 +3,6 @@ class A{
         System.out.println("Hello GIT!");
         System.out.println("updated on local");
         // update by Remote in line 5
+        // update by Local in line 6
     }
 }
