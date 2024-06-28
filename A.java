a816870923e08a14194e2b9f46e5de205ac9c004
rg007891 <86410329+rg007891@users.noreply.github.com>
2024-06-28 13:11:00 +0530

    Update A.java line 6 conflict updated by remote

--- A.java
+++ A.java
@@ -3,10 +3,6 @@ class A{
         System.out.println("Hello GIT!");
         System.out.println("updated on local");
         // update by Remote in line 5
-<<<<<<< HEAD
-        // update by Local in line 6
-=======
         // update by Remote in line 6
->>>>>>> cc7589cd918646e3f723cf24e5d02d1efac47881
     }
 }
